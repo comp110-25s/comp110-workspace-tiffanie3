282c054f2e65b720f8d229a81c900b02a573596a
Tiffanie Lee <tlee@unc.edu>
2025-01-23 22:55:03 -0500

    tea_bags

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -1,3 +1,7 @@
-"""Practice writing multiple small subprograms to create a larger program to plan a tea party"""
+"""Practice with decomposing a problem into smaller subproblems to plan a tea party"""
 
 __author__ = "730471301"
+
+def tea_bags (people: int)
+'''computes number of tea bags based on number of guests'''
+return people*2
