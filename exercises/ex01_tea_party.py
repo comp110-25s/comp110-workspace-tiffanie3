b45b4f2d1a7239e1c8841ec27a8147615076044d
Tiffanie Lee <tlee@unc.edu>
2025-01-23 23:01:23 -0500

    testing out tea_bags

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -3,6 +3,11 @@
 __author__ = "730471301"
 
 
-def tea_bags(people: int) --> int:
+def tea_bags(people: int) -> int:
     """computes number of tea bags based on number of guests"""
     return people * 2
+
+
+tea_bags(people=2)
+
+tea_bags(people=334)
