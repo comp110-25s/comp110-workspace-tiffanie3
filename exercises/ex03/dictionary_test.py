50d87146e473c29f1d773504aef8460020b3c0ea
Tiffanie Lee <tlee@unc.edu>
2025-03-26 07:11:56 -0400

    invert

--- exercises/ex03/dictionary_test.py
+++ exercises/ex03/dictionary_test.py
@@ -3,6 +3,23 @@ __author__ = "730471301"
 from exercises.ex03.dictionary import invert
 
 
-def test_invert() -> None:
-    """Test use case for invert"""
+def test_invert_with_letters() -> None:
+    """Test use case for invert with letters"""
     assert invert({"a": "z", "b": "y", "c": "x"}) == {"z": "a", "y": "b", "x": "c"}
+
+
+def test_invert_with_words() -> None:
+    """Test use case for invert with words"""
+    assert invert({"apple": "cat"}) == {"cat": "apple"}
+
+
+def test_invert_with_empty_input() -> None:
+    """Test edge case for invert with empty input"""
+    assert invert({}) == {}
+
+
+import pytest
+
+with pytest.raises(KeyError):
+    my_dictionary = {"kris": "jordan", "michael": "jordan"}
+    invert(my_dictionary)
