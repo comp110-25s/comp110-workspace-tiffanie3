e2a1e6764564d4a06ad0413266c85746b963f74c
Tiffanie Lee <tlee@unc.edu>
2025-03-26 14:35:48 -0400

    favorite color

--- exercises/ex03/dictionary.py
+++ exercises/ex03/dictionary.py
@@ -12,3 +12,34 @@ def invert(dictionary: dict[str, str]) -> dict[str, str]:
             inverted_dictionary[value] = key
     print(inverted_dictionary)
     return inverted_dictionary
+
+
+def count(list: list[str]) -> dict[str, int]:
+    """counts the number of times a str appears in a given list, returns dictionary of counts for each str"""
+    dictionary_of_counts: dict[str, int] = {}
+    idx: int = 0
+    while idx < len(list):
+        item: str = list[idx]
+        if item in dictionary_of_counts:
+            dictionary_of_counts[item] += 1
+        else:
+            dictionary_of_counts[item] = 1
+        idx += 1
+    print(dictionary_of_counts)
+    return dictionary_of_counts
+
+
+def favorite_color(sample: dict[str, str]) -> str:
+    """returns the most popular color from a dictionary with of names and favorite colors"""
+    color_list: list[str] = []
+    for name in sample:
+        color_list.append(sample[name])
+    dictionary_color: dict[str, int] = count(color_list)
+    idx: int = 0
+    while idx < len(dictionary_color):
+        for color in dictionary_color:
+            color_popular: int = dictionary_color[color_list[idx]]
+            if dictionary_color[color] > color_popular:
+                print(color)
+                return color
+            idx += 1
--- exercises/ex03/dictionary_test.py
+++ exercises/ex03/dictionary_test.py
@@ -1,6 +1,9 @@
 __author__ = "730471301"
 
 from exercises.ex03.dictionary import invert
+import pytest
+from exercises.ex03.dictionary import count
+from exercises.ex03.dictionary import favorite_color
 
 
 def test_invert_with_letters() -> None:
@@ -18,8 +21,40 @@ def test_invert_with_empty_input() -> None:
     assert invert({}) == {}
 
 
-import pytest
-
 with pytest.raises(KeyError):
     my_dictionary = {"kris": "jordan", "michael": "jordan"}
     invert(my_dictionary)
+
+
+def test_count_colors() -> None:
+    """Test use case for count with colors"""
+    list_colors: list[str] = ["red", "blue", "yellow", "red", "blue", "blue"]
+    assert count(list_colors) == {
+        "red": 2,
+        "blue": 3,
+        "yellow": 1,
+    }
+
+
+def test_count_letters() -> None:
+    """Test use case for count with letters"""
+    list: list[str] = ["a", "b", "c", "d", "1", "a"]
+    assert count(list) == {"a": 2, "b": 1, "c": 1, "d": 1, "1": 1}
+
+
+def test_count_empty() -> None:
+    """Test edge case for count with empty list"""
+    list: list[str] = []
+    assert count(list) == {}
+
+
+def test_favorite_color() -> None:
+    """Test use case for favorite_color"""
+    dict_color: dict[str, str] = {
+        "Tiffanie": "blue",
+        "Brandon": "blue",
+        "Elena": "green",
+        "Naomi": "pink",
+        "Kimberly": "teal",
+    }
+    assert favorite_color(dict_color) == "blue"
